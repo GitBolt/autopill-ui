1fb7c0f98f9586fc7f700084b46e69948f73c61f
GitBolt <saabisakhtar@gmail.com>
2023-12-02 20:54:20 +0530

    add: validation

--- src/components/MedicationForm.tsx
+++ src/components/MedicationForm.tsx
@@ -1,22 +1,41 @@
 import { useState } from 'react';
 import {
-  Box, Button, CloseButton, Flex, FormControl, FormLabel, Input, Text, useTheme,
+  Box, Button, CloseButton, Flex, FormControl, FormLabel, Input, Text, useTheme, useToast,
 } from '@chakra-ui/react';
 import TimePicker from 'react-time-picker'
 import { Medication } from '@/types';
 import 'react-time-picker/dist/TimePicker.css';
 import 'react-clock/dist/Clock.css';
+import { AddIcon, SmallAddIcon } from '@chakra-ui/icons'
+
 interface MedicationFormProps {
   onSave: (medication: Medication) => void;
 }
 
 const MedicationForm: React.FC<MedicationFormProps> = ({ onSave }) => {
-  const [medication, setMedication] = useState<Medication>({ name: '', times: [], counter: 0 });
-  const theme = useTheme();
+  const [medication, setMedication] = useState<Medication>({ name: '', times: [new Date().toISOString()], counter: 0 });
+  const toast = useToast();
 
   const handleSave = () => {
-    onSave(medication);
-    setMedication({ name: '', times: [], counter: 0 });
+
+    const allTimesFilled = medication.times.every(time => time !== "");
+
+    if (medication.name == "") {
+      toast({
+        title: 'Error',
+        description: 'Pill name is required',
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      });
+      return
+    }
+    if (allTimesFilled) {
+      onSave(medication);
+      setMedication({ name: '', times: [], counter: 0 });
+    } else {
+
+    }
   };
 
   const removeTimeInput = (index: number) => {
@@ -27,28 +46,28 @@ const MedicationForm: React.FC<MedicationFormProps> = ({ onSave }) => {
 
   const handleTimeChange = (index: number, timeString: string | null) => {
     const newTimes = [...medication.times];
-  
+
     if (timeString) {
       const [hours, minutes] = timeString.split(':').map(Number);
       const dateTime = new Date();
       dateTime.setHours(hours, minutes, 0, 0);
-  
+
       const localISOTime = dateTime.getFullYear() + '-' +
-                           String(dateTime.getMonth() + 1).padStart(2, '0') + '-' +
-                           String(dateTime.getDate()).padStart(2, '0') + 'T' +
-                           String(dateTime.getHours()).padStart(2, '0') + ':' +
-                           String(dateTime.getMinutes()).padStart(2, '0') + ':' +
-                           String(dateTime.getSeconds()).padStart(2, '0');
-  
+        String(dateTime.getMonth() + 1).padStart(2, '0') + '-' +
+        String(dateTime.getDate()).padStart(2, '0') + 'T' +
+        String(dateTime.getHours()).padStart(2, '0') + ':' +
+        String(dateTime.getMinutes()).padStart(2, '0') + ':' +
+        String(dateTime.getSeconds()).padStart(2, '0');
+
       newTimes[index] = localISOTime;
       console.log(localISOTime)
     } else {
       newTimes[index] = '';
     }
-  
+
     setMedication({ ...medication, times: newTimes });
   };
-  
+
 
   const addTimeInput = () => {
     if (medication.times.length < 5) {
@@ -79,12 +98,11 @@ const MedicationForm: React.FC<MedicationFormProps> = ({ onSave }) => {
         </Box>
       ))}
 
-      <Flex w="80%">
+      <Flex w="80%" align="center">
         {medication.times.length ? <Button w="100%" mt={4} colorScheme="blue" onClick={handleSave}>Save</Button> : null}
         {medication.times.length < 5 && (
-          <Button mt={4} ml={4} colorScheme="green" onClick={addTimeInput} w="100%">
-            Add Time
-          </Button>
+          <AddIcon mt={4} ml={4} onClick={addTimeInput} cursor="pointer" />
+
         )}
       </Flex>
 
